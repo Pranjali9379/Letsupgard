8471868a657a95705f764902d4272faa4c8911e6
Pranjali9379 <70760306+Pranjali9379@users.noreply.github.com>
2020-10-11 18:30:43 +0530

    assignment1 java

---
+++ letupgrade/src/assignment1/Employee.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class Employee {
+
+	int age;
+	String name;
+	String city;
+	
+	
+	public void display() {
+		
+		System.out.println("The name is "+name);
+		System.out.println("The age is "+age);
+		System.out.println("The city is "+city);
+	}
+}
---
+++ letupgrade/src/assignment1/Main.java
@@ -0,0 +1,21 @@
+package assignment1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Employee e=new Employee();
+        e.age=23;
+        e.name="Yogesh";
+        e.city="Pune";
+         e.display();
+         
+       Employee e1=new Employee();
+        e1.age=56;
+         e1.name="Rakash";
+         e1.city="Banglore";    
+         e1.display();
+         
+	}
+
+}
